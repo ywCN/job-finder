c67c98921c00578be8052823aa00ce9a99d49477
ywCN <wangyouhao90@gmail.com>
2018-01-13 12:31:49 -0500

    set up Navigator

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        // define some routes for screens
+        const MainNavigator = TabNavigator({
+            welcome: { screen: WelcomeScreen },
+            auth: { screen: AuthScreen }
+        });
+        return (
+            <View style={styles.container}>
+                <MainNavigator />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+        // alignItems: 'center',
+        // justifyContent: 'center'
+    }
 });
