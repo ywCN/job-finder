2a92dc747899befd1d3f56e1335fde52e2917f93
ywCN <wangyouhao90@gmail.com>
2018-01-13 13:01:55 -0500

    header config

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+    // Navigator will check navigationOptions property of this class and use it
+    static navigationOptions = ({ navigation }) => ({
+        title: 'Review Jobs',
+        headerRight: (
+            <Button
+                title="Settings"
+                onPress={() => navigation.navigate('settings')}
+            />
+        )
+    });
     render() {
         return (
             <View>
