be86746d871e79f0ee0ebc351c5a1b86f2d6cfb8
ywCN <wangyouhao90@gmail.com>
2018-01-13 12:43:48 -0500

    nest Navigator for main flow

--- App.js
+++ App.js
@@ -4,13 +4,22 @@ import { TabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 export default class App extends React.Component {
     render() {
         // define some routes for screens
+        // keys like welcome and auth will become tab names
         const MainNavigator = TabNavigator({
             welcome: { screen: WelcomeScreen },
-            auth: { screen: AuthScreen }
+            auth: { screen: AuthScreen },
+            main: {
+                screen: TabNavigator({
+                    map: { screen: MapScreen },
+                    deck: { screen: DeckScreen }
+                })
+            }
         });
         return (
             <View style={styles.container}>
