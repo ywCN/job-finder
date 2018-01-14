db0525a7aff8998a9a5b664acf166deb27b0d549
ywCN <wangyouhao90@gmail.com>
2018-01-13 22:34:16 -0500

    new component

---
+++ components/Slides.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Slides extends Component {
+    render() {
+        return <View />;
+    }
+}
+
+export default Slides;
