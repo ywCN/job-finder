8eb7d05aa2244e553c6a7e5994120a38fc7020c7
ywCN <wangyouhao90@gmail.com>
2018-01-13 22:53:51 -0500

    add and use color

--- components/Slides.js
+++ components/Slides.js
@@ -7,7 +7,13 @@ class Slides extends Component {
     renderSlides() {
         return this.props.data.map(slide => {
             return (
-                <View key={slide.text} style={styles.slideStyle}>
+                <View
+                    key={slide.text}
+                    style={[
+                        styles.slideStyle,
+                        { backgroundColor: slide.color }
+                    ]}
+                >
                     <Text style={styles.textStyle}>{slide.text}</Text>
                 </View>
             );
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -4,8 +4,9 @@ import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
-    { text: 'Welcome to Job Finder' },
-    { text: 'Set your location, then swipe away' }
+    { text: 'Welcome to Job Finder', color: '#03A9F4' },
+    { text: 'Use the app to find a job', color: '#009688' },
+    { text: 'Set your location, then swipe away', color: '#03A9F4' }
 ];
 
 class WelcomeScreen extends Component {
