418f37ff4cd3c8e20ab94a4a2d1be696f45918fb
ywCN <wangyouhao90@gmail.com>
2018-01-14 11:35:10 -0500

    fix typo

--- store/index.js
+++ store/index.js
@@ -4,6 +4,6 @@ import thunk from 'redux-thunk';
 
 import reducers from '../reducers';
 
-const store = create(reducers, {}, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
 
 export default store;
