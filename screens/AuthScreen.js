06bb4923b1a8607665c2a51ab25e392d9c0fd59b
ywCN <wangyouhao90@gmail.com>
2018-01-14 20:25:48 -0500

    clean up View

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -23,15 +23,7 @@ class AuthScreen extends Component {
     }
 
     render() {
-        return (
-            <View>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-            </View>
-        );
+        return <View />;
     }
 }
 
