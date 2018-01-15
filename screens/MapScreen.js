7712af9faf59c64c898d1003fd33e57477db99e0
ywCN <wangyouhao90@gmail.com>
2018-01-15 15:05:41 -0500

    add button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
 
 import * as actions from '../actions';
 
@@ -41,9 +42,27 @@ class MapScreen extends Component {
                     style={{ flex: 1 }}
                     onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+                <View style={styles.buttonContainer}>
+                    <Button
+                        large
+                        title="Search This Area"
+                        backgroundColor="#009688"
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPress}
+                    />
+                </View>
             </View>
         );
     }
 }
+
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0
+    }
+};
 
 export default connect(null, actions)(MapScreen);
