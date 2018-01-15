cd8e21ff959017e8122a631c905f39b4a9132bd2
ywCN <wangyouhao90@gmail.com>
2018-01-14 22:29:58 -0500

    build query string for request

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
 
 import { FETCH_JOBS } from './types';
+
+const JOB_QUERY_PARAMS = {
+    publisher: '4201738803816157',
+    format: 'json',
+    v: '2',
+    latlong: 1,
+    radius: 10,
+    q: 'javascript'
+};
 
 export const fetchJobs = region => async dispatch => {
     try {
