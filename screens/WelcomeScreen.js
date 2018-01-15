752cb38efd1f2970ce82ba3a603ff40797266b7d
ywCN <wangyouhao90@gmail.com>
2018-01-14 21:10:55 -0500

    debugged: setState after you have navigate out of the component source: https://www.udemy.com/react-native-advanced/learn/v4/questions/2357722

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -18,8 +18,8 @@ class WelcomeScreen extends Component {
         let token = await AsyncStorage.getItem('fb_token');
 
         if (token) {
-            this.props.navigation.navigate('map');
             this.setState({ token });
+            this.props.navigation.navigate('map');
         } else {
             this.setState({ token: false });
         }
