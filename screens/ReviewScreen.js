649e11dc9bae75113536b3466e7775347326ade7
ywCN <wangyouhao90@gmail.com>
2018-01-15 16:29:00 -0500

    renderLikedJobs

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
+import { View, Text, Platform, ScrollView } from 'react-native';
 import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
     // Navigator will check navigationOptions property of this class and use it
@@ -19,17 +20,70 @@ class ReviewScreen extends Component {
             marginTop: Platform.OS === 'android' ? 24 : 0
         }
     });
+
+    renderLikedJobs() {
+        return this.props.likedJobs.map(job => {
+            const {
+                company,
+                formattedRelativeTime,
+                url,
+                longitude,
+                latitude,
+                jobtitle,
+                jobkey
+            } = job;
+            const initialRegion = {
+                longitude,
+                latitude,
+                latitudeDelta: 0.045,
+                longitudeDelta: 0.02
+            };
+
+            return (
+                <Card title={jobtitle} key={jobkey}>
+                    <View style={{ height: 200 }}>
+                        <MapView
+                            style={{ flex: 1 }}
+                            cacheEnabled={Platform.OS === 'android'}
+                            scrollEnabled={false}
+                            initialRegion={initialRegion}
+                        />
+                        <View style={styles.detailWrapper}>
+                            <Text style={styles.italics}>{company}</Text>
+                            <Text style={styles.italics}>
+                                {formattedRelativeTime}
+                            </Text>
+                        </View>
+                        <Button
+                            title="Apply Now!"
+                            backgroundColor="#03A9F4"
+                            onPress={() => Linking.openURL(url)}
+                        />
+                    </View>
+                </Card>
+            );
+        });
+    }
+
     render() {
-        return (
-            <View>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-            </View>
-        );
+        return <ScrollView>{this.renderLikedJobs()}</ScrollView>;
     }
 }
 
-export default ReviewScreen;
+const styles = {
+    italics: {
+        fontStyle: 'italic'
+    },
+    detailWrapper: {
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+};
+
+function mapStateToProps(state) {
+    return { likedJobs: state.likedJobs };
+}
+
+export default connect(mapStateToProps)(ReviewScreen);
