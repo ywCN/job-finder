9ae649fff46e8e16311ebe590c9d431136f2a511
ywCN <wangyouhao90@gmail.com>
2018-01-14 21:30:21 -0500

    fix map issue with Android

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
     // init state
     state = {
+        mapLoaded: false,
         region: {
             longitude: -122,
             latitude: 37,
@@ -13,7 +14,18 @@ class MapScreen extends Component {
         }
     };
 
+    componentDidMount() {
+        this.setState({ mapLoaded: true });
+    }
+
     render() {
+        if (!this.state.mapLoaded) {
+            return (
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        }
         return (
             <View style={{ flex: 1 }}>
                 <MapView region={this.state.region} style={{ flex: 1 }} />
