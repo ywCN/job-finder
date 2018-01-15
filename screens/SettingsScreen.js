7e7d1118263a2ff29dc15bbc2ce7b68dcb36f310
ywCN <wangyouhao90@gmail.com>
2018-01-15 16:54:45 -0500

    clearing liked

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import { clearLikedJobs } from '../actions';
 
 class SettingsScreen extends Component {
     render() {
         return (
             <View>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
-                <Text>SettingsScreen</Text>
+                <Button
+                    title="Reset Liked Jobs"
+                    large
+                    icon={{ name: 'delete-forever' }}
+                    backgroundColor="#F44336"
+                    onPress={this.props.clearLikedJobs}
+                />
             </View>
         );
     }
 }
 
-export default SettingsScreen;
+export default connect(null, { clearLikedJobs })(SettingsScreen);
