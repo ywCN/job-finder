fd14c9a14d6e2ba73a2df1c618414340d6fd8415
ywCN <wangyouhao90@gmail.com>
2018-01-14 22:25:47 -0500

    convert geocode to zip code

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
 
 import { FETCH_JOBS } from './types';
 
-export const fetchJobs = region => async dispatch => {};
+export const fetchJobs = region => async dispatch => {
+    try {
+        let zip = await reverseGeocode(region);
+    } catch (e) {
+        console.error(e);
+    }
+};
