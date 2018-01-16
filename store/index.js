67bf000e47d9ec4c827d1f398fb70ed74df8f781
ywCN <wangyouhao90@gmail.com>
2018-01-15 20:17:20 -0500

    downgrade redux persist to 4.6.0

--- store/index.js
+++ store/index.js
@@ -1,9 +1,17 @@
 // This is redux store
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 
 import reducers from '../reducers';
 
-const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
+const store = createStore(
+    reducers,
+    {},
+    compose(applyMiddleware(thunk), autoRehydrate())
+);
+
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 
 export default store;
